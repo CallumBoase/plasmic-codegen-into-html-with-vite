86f371e70b3355a150e125c86a76f426270e3dfa
CallumBoase <callum.boase@gmail.com>
2024-05-13 14:06:55 +1000

    cleanup vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   define: {
     'process.env.NODE_ENV': JSON.stringify(mode),
@@ -15,17 +15,5 @@ export default defineConfig(({ command, mode }) => ({
       // the proper extensions will be added
       fileName: 'customComponents',
     },
-    // rollupOptions: {
-    //   // make sure to externalize deps that shouldn't be bundled
-    //   // into your library
-    //   external: ['vue'],
-    //   output: {
-    //     // Provide global variables to use in the UMD build
-    //     // for externalized deps
-    //     globals: {
-    //       vue: 'Vue',
-    //     },
-    //   },
-    // },
   },
 }))
