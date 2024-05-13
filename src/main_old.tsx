0fb0c30b0b6a14be8190d5ea62e2ab2f437fedee
CallumBoase <callum.boase@gmail.com>
2024-05-13 12:19:04 +1000

    First working

--- src/main_old.tsx
+++ src/main_old.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
 import './index.css'
 
 import { NavigationBar } from './components/NavigationBar'
 
+// const domNode = document.getElementById('navigation')
 
+declare global {
+  interface Window {
+    customComponents: {
+      render: {
+        navigationBar: (targetDivId: string) => void
+      }
+    }
+  }
+}
 
-const customComponents = {
+window.customComponents = {
   render: {
-    navigationBar: (targetDivId: string) => {
+    navigationBar: (targetDivId) => {
       ReactDOM.createRoot(document.getElementById(targetDivId)!).render(
         <React.StrictMode>
           <NavigationBar />
@@ -17,8 +28,6 @@ const customComponents = {
     }
   }
 }
-
-export default customComponents
 
 // ReactDOM.createRoot(domNode!).render(
 //   <React.StrictMode>
